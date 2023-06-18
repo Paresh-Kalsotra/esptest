bbfac2199df04f059028bd2d9cffdc571da2666d
KHAL221 <hamza.ah190@gmail.com>
2023-06-19 02:04:24 +0530

    first commit

---
+++ api/routes/products.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+
+const Product = require('./models/product')
+
+router.get('/', (req,res,next)=>{
+    res.status(200).json({
+        message : 'Handling Get reqs to /products'
+    })
+})
+
+router.post('/', (req,res,next)=>{
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    })
+    product
+    .save()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err))
+    res.status(201).json({
+        message : 'Handling Post reqs to /products'
+    })
+})
+
+router.patch('/:productId', (req,res,next)=>{
+    res.status(200).json({
+        message : 'Deleted updated'
+    })
+})
+router.delete('/:productId', (req,res,next)=>{
+    res.status(200).json({
+        message : 'Deleted product'
+    })
+})
+module.exports = router;
