d452745e9c4ec98fa2a364015b80d52d8b588a40
KHAL221 <hamza.ah190@gmail.com>
2023-06-19 11:17:44 +0530

    yes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,7 +9,7 @@ router.get('/',(req,res,next)=>{
 
 router.post('/',(req,res,next)=>{
     const order = {
-        productOd: req.body.productId,
+        productId: req.body.productId,
         quantity: req.body.quantity
     }
     res.status(201).json({
