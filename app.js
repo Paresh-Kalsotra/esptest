8c619df0f45e3d8d5bf48764af960a808d8e4dc7
KHAL221 <hamza.ah190@gmail.com>
2023-06-19 15:27:43 +0530

    yes

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ require('dotenv').config()
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-try{
-    await mongoose.connect(process.env.mongoUri)
-}
-catch(error){
-    handleError(error)
-}
+(async()=>{
+    try{
+        await mongoose.connect(process.env.mongoUri)
+    }
+    catch(error){
+        handleError(error)
+    }
+})()
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
