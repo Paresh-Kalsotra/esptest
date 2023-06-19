0a1d2d8f64ce88867e98003c7503c225363c09d4
KHAL221 <hamza.ah190@gmail.com>
2023-06-19 14:13:00 +0530

    yes

--- app.js
+++ app.js
@@ -21,15 +21,6 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.use((req, res, next)=>{
-    res.header('Acess-Control-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    if(req.method === 'OPTIONS'){
-        res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-        return res.status(200).json({})
-    }
-    next();
-})
 
 app.get('/',(req,res,next)=>{
     res.status(200).json({
