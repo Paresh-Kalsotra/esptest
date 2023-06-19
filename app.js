3b3d1ad8754cd95f3001e44532610312b494bd17
KHAL221 <hamza.ah190@gmail.com>
2023-06-19 10:47:31 +0530

    yes

--- app.js
+++ app.js
@@ -26,7 +26,11 @@ app.use((req, res, next)=>{
 
 app.use('/products',productRoutes)
 app.use('/orders', orderRoutes)
-
+app.get('/', (res,req)=>{
+    res.status(200).json({
+        message: "home"
+    })
+})
 app.use((req,res,next)=>{
     const error = new Error('Not found');
     error.status = 404;
