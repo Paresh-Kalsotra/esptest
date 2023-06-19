bd1c2e3e06ea7079e711e4b2b9a96f2f1350f493
KHAL221 <hamza.ah190@gmail.com>
2023-06-19 14:11:19 +0530

    yes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const app = require('./app')
+const mongoose = require('mongoose')
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app)
 
-server.listen(port)
+connectDB().then(()=>{
+    server.listen(port)
+})
